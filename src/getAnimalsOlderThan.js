7c24062732c555e90706384f3dd081c151a2ff78
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-14 17:26:30 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((bicho) => bicho.name === animal)
+    .residents.every((residente) => residente.age > age);
 }
+
+/* A função primeiro vai acessar o objeto data e o array species para buscar um bicho que tenha um nome
+igual ao do animal recebido no parâmetro. A partir disso, irá acessar o array residents para verficar
+dentre todos os animais residentes, a partir de sua idade, quais deles possuem a idade maior que a age passada no parâmetro.
+*/
 
 module.exports = getAnimalsOlderThan;
