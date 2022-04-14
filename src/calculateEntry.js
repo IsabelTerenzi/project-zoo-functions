261020d1320c0f14e8ddbcd6d198a96e7f5a8e26
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-14 16:34:54 -0300

    requisito 1

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,7 +5,7 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui oi
+  // seu código aqui
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) =>
+  data.species.filter((especie) => ids.includes(especie.id));
+
+/* A função vai receber um parâmetro rest, pois pode receber um ou mais ids.
+ Então eu vou acessar meu objeto data, que contém um array de objetos species.
+ Então vou filtrar para verificar se esse array species contém a chave id em seus objetos.
+ O includes faz o link entre o parâmetro ids e o parâmetro espécie.
+ */
 
 module.exports = getSpeciesByIds;
