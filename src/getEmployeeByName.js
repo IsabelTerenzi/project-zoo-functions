a5b83a0a3902214958072c4b62cf4b847d41cda6
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-15 18:08:50 -0300

    requisito 3 comentado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -9,4 +9,10 @@ function getEmployeeByName(employeeName) {
     ) : {};
 }
 
+/* A função vai receber o nome de um funcionário, podendo ser o primeiro nome ou o sobrenome, e vai
+retornar o objeto daquele funcionário. O find irá buscar o funcionário que satisfaça a condição passada
+pelo nome ou sobrenome, dentro do objeto employees. Se houver um primeiro nome ou um sobrenome, irá retornar
+o objeto do funcionário, senão, irá retornar um objeto vazio.
+*/
+
 module.exports = getEmployeeByName;
