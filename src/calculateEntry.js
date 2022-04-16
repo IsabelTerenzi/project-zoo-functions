72f765d9a45888b11d0a2bf8d6fd63fa1831f0f3
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-16 12:10:58 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((pessoa) => {
+    if (pessoa.age < 18) child += 1;
+    if (pessoa.age >= 18 && pessoa.age < 50) adult += 1;
+    if (pessoa.age >= 50) senior += 1;
+  });
+  return { child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const crianca = data.prices.child;
+  const adulto = data.prices.adult;
+  const idoso = data.prices.senior;
+  const { child, adult, senior } = countEntrants(entrants);
+  return (crianca * child) + (adulto * adult) + (idoso * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
