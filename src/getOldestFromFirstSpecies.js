9d5e425665c23cb442d810e304c66667966b0db3
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-16 12:59:54 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = data.employees.find((funcio) => funcio.id === id).responsibleFor[0];
+  const animal = data.species.find((bicho) => bicho.id === funcionario).residents;
+  const maisVelho = animal.reduce((acc, animalzinho) => {
+    if (acc.age < animalzinho.age) {
+      return animalzinho;
+    }
+    return acc;
+  });
+  return Object.values(maisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
