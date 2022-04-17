40c6923f69646a61cb1b9f5de02d67aa03cccaf8
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-17 16:54:35 -0300

    requisito 6 explicado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -15,6 +15,12 @@ function countEntrants(entrants) {
   };
 }
 
+/* A função countEntrants irá receber um contador para cada faixa etária, child, adult e senior.
+Para cada pessoa do parâmetro passado eu vou receber uma pessoa e verificar em qual faixa etária
+ela se encontra, adicionando ao contador de cada uma delas. Então, vou retornar um objeto com as
+3 faixas etárias e a quantidade de pessoas do contador.
+*/
+
 function calculateEntry(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) {
     return 0;
@@ -26,4 +32,10 @@ function calculateEntry(entrants) {
   return (crianca * child) + (adulto * adult) + (idoso * senior);
 }
 
+/* A função calculateEntry irá receber os visitantes como parâmetro e caso não haja parâmetro ou
+o tamanho das chaves do objeto recebido seja zero, então irá retornar 0.
+Vou então acessar o objeto prices de data e estabelecer o preço para cada faixa etária. Então vou atribuir
+à função o objeto child, adult e senior. Então retornar a quantidade de pessoas de cada faixa etária vezes
+o preço da entrada de cada um deles, o valor final dessa conta.
+*/
 module.exports = { calculateEntry, countEntrants };
