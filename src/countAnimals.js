f8d12fb7d62dad89e2a0fff77d613ed9c7681c35
Isabel Terenzi <bel.terenzi@gmail.com>
2022-04-17 16:44:56 -0300

    requisito 5 explicado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -6,6 +6,11 @@ function animalNulo() {
   return objeto;
 }
 
+/* A função animalNulo será retornada caso não haja nenhum parâmetro na função principal. Para isso,
+vou acessar as espécies e para cada bicho, vou retornar um objeto que contenha o nome do animal
+e o tamanho do seu array de residentes, ou seja, quantos animais eu tenho daquele bicho.
+*/
+
 function quantAnimais(objeto) {
   if (objeto) {
     return data.species.find((bicho) => Object.values(objeto)
@@ -13,16 +18,28 @@ function quantAnimais(objeto) {
   }
 }
 
+/* A função quantAnimais irá receber um objeto como parâmeto e irá buscar dentre os animais,
+os valores das chaves daquele objeto recebido como parâmetro e verificar se nele existe o nome do animal.
+E então retornará o tamanho do array de residentes daquele animal.
+*/
+
 function animalSexo(objeto) {
   let contador = 0;
   data.species.find((bicho) => Object.values(objeto)[0]
-    .includes(bicho.name)).residents.forEach((contar) => {
-    if (contar.sex === Object.values(objeto)[1]) {
+    .includes(bicho.name)).residents.forEach((residente) => {
+    if (residente.sex === Object.values(objeto)[1]) {
       contador += 1;
     }
   });
   return contador;
 }
+
+/* A função animalSexo irá receber um objeto como parâmetro e irá buscar dentre os animais, o valor
+das chaves do objeto recebido na primeira posição do array. Irá então verificar se esse objeto, nessa posição
+inclui o nome do animal, então irá pegar o array de residentes e para cada residente, irá verificar se o sexo
+do animal é igual à segunda posição do objeto e então adicionar um ao contador, retornando a quantidade de
+animais daquele sexo passado no parâmetro.
+*/
 
 function countAnimals(animal) {
   if (!animal) return animalNulo();
@@ -31,4 +48,8 @@ function countAnimals(animal) {
   if (specie) return quantAnimais(animal);
 }
 
+/* A função principal irá reunir todas as outras funções, ou seja, se não houver parâmetro, retornar a função
+animalNulo. Acessando as chaves specie e sex do objeto animal recebido no parâmetro, caso haja as duas chaves,
+retorna a função animalSexo. Caso haja apenas a chave specue, retorna a função quantAnimais.
+*/
 module.exports = countAnimals;
